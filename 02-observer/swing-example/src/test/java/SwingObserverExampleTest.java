27535e7fa6e7306f6e5f17d50991692f0b1aadde
Bulbo <bolsonbulbo@gmail.com>
2019-11-09 19:53:05 +0100

    added swing example

---
+++ 02-observer/swing-example/src/test/java/SwingObserverExampleTest.java
@@ -0,0 +1,11 @@
+import swingexample.*;
+import org.junit.Test;
+
+public class SwingObserverExampleTest {
+
+    @Test
+    public void ShouldDisplayMessageWhenClick() {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+}
